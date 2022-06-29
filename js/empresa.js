d216ff45205c22fb0696fdfee67343a1e2a2f52e
Souzlipes <78570260+Souzlipes@users.noreply.github.com>
2022-06-28 22:38:30 -0300

    first commit

---
+++ js/empresa.js
@@ -0,0 +1,81 @@
+// link Data base
+URL = 'https://storage-epets.herokuapp.com/empresa';
+
+//Get Empresa
+const petShopList = document.getElementById('petShopList');
+const form  = document.getElementById('form');
+const search = document.getElementById('search');
+
+fetch(URL)
+    .then(res => res.json())
+    .then(empresa => {
+        let petShopData = "";
+        for(let i = 0; i < empresa.length; i++){
+            petShopData += `
+                <div class="row produtos col-12 col-md-6 col-lg-3">
+                    <img src="${empresa[i].img}" alt="produtos">
+                    <div class="infor_loja">
+                        <p>Petz</p>
+                        <span>CEP: ${empresa[i].cep} - ${empresa[i].rua}, ${empresa[i].num} - ${empresa[i].complemento} - ${empresa[i].bairro}, ${empresa[i].cidade} - ${empresa[i].uf}</span>
+                    </div>
+                </div>
+            `;
+            petShopList.innerHTML = petShopData;
+        }
+    })
+
+function buscaCEP(cep) {
+
+    var cep = cep.slice(0, 2);
+    console.log(cep);
+    fetch(URL)
+        .then(res => res.json())
+        .then(empresa => {
+            let petShopData = "";
+            for (let i = 0; i < empresa.length; i++) {
+                if (empresa[i].cep.startsWith(cep)) {
+                    petShopData += `
+                <div class="row produtos col-12 col-md-6 col-lg-3">
+                    <img src="${empresa[i].img}" alt="produtos">
+                    <div class="infor_loja">
+                        <p>Petz</p>
+                        <span>CEP: ${empresa[i].cep} - ${empresa[i].rua}, ${empresa[i].num} - ${empresa[i].complemento} - ${empresa[i].bairro}, ${empresa[i].cidade} - ${empresa[i].uf}</span>
+                    </div>
+                </div>
+            `;
+                    petShopList.innerHTML = petShopData;
+                }
+            }
+        })
+}
+
+
+//post PETshop
+const cadastroEMP = document.getElementById('dados_emp');
+
+function postLoja() {
+
+    const empresa = JSON.stringify({
+        id: Math.floor(Math.random() * 10),
+        img: document.getElementById('img').value,
+        telefone: document.getElementById('telefone').value,
+        cep: document.getElementById('cep').value,
+        rua: document.getElementById('rua').value,
+        bairro: document.getElementById('bairro').value,
+        cidade: document.getElementById('cidade').value,
+        uf: document.getElementById('uf').value,
+        num: document.getElementById('num').value,
+        complemento: document.getElementById('complemento').value
+
+    })
+
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: empresa
+    })
+        .then(res => res.json())
+        .then(() => location.reload);
+}
---
+++ js/main.js
@@ -0,0 +1,78 @@
+// link Data base
+URL = 'https://storage-epets.herokuapp.com/usuario';
+
+//GET login
+
+function loginUser(){
+    fetch(URL)
+    .then(res => res.json())
+    .then(usuario =>{
+        let email_in = document.getElementById('in_email').value;
+        let password_in = document.getElementById('in_password').value;
+
+        var validar_email = new Boolean ();
+        var validar_senha = new Boolean();
+        
+        for(i = 0; i<usuario.length; i++){            
+            validar_email = (verificaEmail(email_in, usuario[i].email));
+            console.log(email_in, validar_email);
+            if(validar_email == true){
+                break;
+            }                        
+        }
+        
+        for(i = 0; i<usuario.length; i++){            
+            validar_senha = (verificaSenha(password_in, usuario[i].password));
+            console.log(email_in, validar_email);
+            if(validar_senha == true){
+                break;
+            }                        
+        }    
+
+        if(validar_email == true && validar_senha == true){
+            window.open("http://127.0.0.1:5502/src/pag/pet_shops.html");
+        }else{
+            alert("Login invalido");
+        } 
+    }); 
+}
+
+
+function verificaEmail(email, emailBD){
+    if(email == emailBD){
+        return true;
+    }
+    return false;
+}
+function verificaSenha(password, passwordBD){
+    if(password = passwordBD){
+        return true;
+    }
+    return false;
+}
+
+
+//POST Cadastro de usuarios
+const cadastroForm = document.getElementById('login-form');
+
+function createUser(){
+    console.log("passando");
+    const usuario = JSON.stringify({
+        id: Math.floor(Math.random() * 10),
+        email: document.getElementById('email').value,
+        cep: document.getElementById('cep').value,
+        cnpj: document.getElementById('cnpj').value,
+        password: document.getElementById('password').value,
+        opcao: document.getElementById('opcao').value           
+    })
+
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: usuario
+    })
+    .then(res => res.json())
+    .then(() =>location.reload);
+}
